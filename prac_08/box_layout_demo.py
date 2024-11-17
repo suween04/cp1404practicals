50670ccdc78a0552d69632ed365d2610ebdc7c6b
suweenjayawickrema <suweenanushka.sudrikajayawickrema@my.jcu.edu.au>
2024-11-17 22:04:28 +1000

    add prac 08

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -7,12 +7,10 @@ class YourApp(App):
         return Builder.load_file('box_layout.kv')
 
     def handle_greet(self):
-        # Get the name from the TextInput and display greeting
         name = self.root.ids.input_name.text
         self.root.ids.output_label.text = f"Hello {name}"
 
     def handle_clear(self):
-        # Reset the text input and label when Clear is clicked
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = ''
 
